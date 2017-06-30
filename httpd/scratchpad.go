d8539ff5d6f9431692d08c054144da14b1657a01
Alexander Klauer <Alexander.Klauer@googlemail.com>
2017-06-30 18:05:01 +0200

    Add scratchpad type for shared data

---
+++ httpd/scratchpad.go
@@ -0,0 +1,63 @@
+/*
+Copyright (c) 2017 Alexander Klauer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package main
+
+import(
+	"sync/atomic"
+	"time"
+)
+
+type scratchpadType struct {
+	// Time is the update time.
+	Time time.Time
+
+	// Data is the scratchpad data.
+	Data []byte
+}
+
+type Scratchpad struct {
+	atomic.Value
+}
+
+func NewScratchpad() *Scratchpad {
+	return &Scratchpad{
+	}
+}
+
+func ( sp *Scratchpad ) Update( data []byte ) {
+	newvalue := scratchpadType{
+		Time: time.Now(),
+		Data: data,
+	}
+	sp.Value.Store( newvalue )
+}
+
+func ( sp *Scratchpad ) Get() ( time.Time, []byte ) {
+	value := sp.Value.Load()
+	if value == nil {
+		return time.Time{}, nil
+	}
+	st := value.( scratchpadType )
+
+	return st.Time, st.Data
+}
